34ae0e1abe69f67b298cba4d0b67255cf948cb9c
pluvia9 <63806056+pluvia9@users.noreply.github.com>
2023-05-27 22:20:29 +0800

    Create fcousclock.py

---
+++ fcousclock.py
@@ -0,0 +1,11 @@
+import time
+
+def focus_timer(minutes: int):
+    seconds = minutes * 60
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timer = "{:02d}:{:02d}".format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+    print("Time's up!")
